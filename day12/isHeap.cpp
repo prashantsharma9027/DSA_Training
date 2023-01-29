7f895200517e9135ce3a4b16f0e16c65ed39ba96
Prashant Sharma <91558085+prashantsharma9027@users.noreply.github.com>
2023-01-29 20:38:24 +0530

    Update isHeap.cpp

--- day12/isHeap.cpp
+++ day12/isHeap.cpp
@@ -24,7 +24,7 @@ bool isMaxHeap(BinaryTreeNode<int> *root)
 int main()
 {
     BinaryTreeNode<int> *root = taktInputLevelorder();
-    cout << isMaxHeap(root);
+    cout  << isMaxHeap(root);
     return 0;
     
 }
